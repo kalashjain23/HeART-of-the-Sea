b874918931669130d6833c162a3faf3f76f1ec07
kalashjain23 <kalashjain513@gmail.com>
2022-05-14 23:48:41 +0530

    changed background

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import back
 import pygame
 import sys
 
@@ -8,31 +9,35 @@ pygame.display.set_caption("Huntin' The Pearl")
 font = pygame.font.Font('font/Pixeltype.ttf', 50)
 clock = pygame.time.Clock()
 
-background = pygame.image.load('graphics/background.png').convert()
-text = font.render('Score:', False, (64, 64, 64))
+sea = pygame.image.load('graphics/water.png').convert()
+
+sky = pygame.transform.scale(pygame.image.load('graphics/background.jpg'). convert(), (900, 700))
+
+text = font.render('Score:', False, (64, 64, 100))
 
 kraken = pygame.transform.scale(pygame.image.load('graphics/kraken/kraken.png'), (85,80)).convert_alpha()
 kraken_rect = kraken.get_rect(topleft = (600, 320))
 
 black_pearl = pygame.transform.scale(pygame.image.load('graphics/ship/black_pearl.png').convert_alpha(), (200, 175))
-pearl_rect = black_pearl.get_rect(topleft = (0, 320))
+pearl_rect = black_pearl.get_rect(topleft = (0, 240))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    screen.blit(background, (0, 0))
 
-    kraken_rect.x -= 4
-    screen.blit(kraken, (kraken_rect.x, 385))
-    if kraken_rect.x < -60 : kraken_rect.x = 800
+    screen.blit(sky, (0, 0))
 
     screen.blit(black_pearl, pearl_rect)
+
+    screen.blit(sea, (0, 400))
+
+    kraken_rect.x -= 4
+    screen.blit(kraken, (kraken_rect.x, 355))
+    if kraken_rect.x < -60 : kraken_rect.x = 800
 
     screen.blit(text, (20, 20))
 
     pygame.display.update()
     clock.tick(60)
-
