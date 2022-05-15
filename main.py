efa1a2f8c9d8052f383076428f93549881a7de6a
kalashjain23 <kalashjain513@gmail.com>
2022-05-15 14:22:11 +0530

    added new enemies, managed enemies spawning and collision with enemies

--- main.py
+++ main.py
@@ -1,38 +1,82 @@
 # Importing Libraries
-from itertools import count
-from turtle import back
+from random import randint
 import pygame
 import sys
 
 
 pygame.init()
 
-# Creating display and font
+# Creating display, font and variables
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Huntin' The Pearl")
+pygame.display.set_caption("HeART of the Sea")
 font = pygame.font.Font('font/Pixeltype.ttf', 50)
 clock = pygame.time.Clock()
+game_active = True
+start_time = 0
+score = 0
 
 # Function to count score
 def count_score():
-    current_time = pygame.time.get_ticks() // 1000
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time 
     score = font.render(f'Score: {current_time}', False, (64, 64, 100))
     score_rect = score.get_rect(center = (400, 50))
     screen.blit(score, score_rect)
+    return current_time
+
+# Collision function
+def collisions(obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if pearl_rect.colliderect(obstacle_rect): return False
+    return True
+
+# Obstacle movement function
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.top == 355: screen.blit(kraken, obstacle_rect)
+            else: screen.blit(fly, obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+    
+    else: return []
 
 # Loading images
 sea = pygame.image.load('graphics/water.png').convert()
-
 sky = pygame.transform.scale(pygame.image.load('graphics/background.jpg'). convert(), (900, 700))
 
+# Obstacles
 kraken = pygame.transform.scale(pygame.image.load('graphics/kraken/kraken.png'), (85,80)).convert_alpha()
-kraken_rect = kraken.get_rect(topleft = (600, 320))
 
+fly = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+
+obstacle_rect_list = []
+
+# Ship
 black_pearl = pygame.transform.scale(pygame.image.load('graphics/ship/black_pearl.png').convert_alpha(), (200, 175))
 pearl_rect = black_pearl.get_rect(topleft = (0, 240))
 
+# Intro screen
+player = pygame.image.load('graphics/player/player.png')
+player = pygame.transform.rotozoom(player, 0, 1.5)
+player_rect = player.get_rect(center = (400, 280))
+
+game_name = font.render("HeART Of The Sea", False, (111, 196, 169))
+game_name = pygame.transform.scale2x(game_name)
+game_name_rect = game_name.get_rect(center = (400, 75))
+
+game_message = font.render("Press SPACE to start.", False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 550))
+
 # for shooting
-bullet = 
+#bullet = 
+
+# Enemy Spawning
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
 
 # The Game Loop
 while True:
@@ -41,24 +85,42 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Getting the images on the screen
-    screen.blit(sky, (0, 0))
+        if game_active:
+            if event.type == obstacle_timer:
+                if randint(0,2): obstacle_rect_list.append(kraken.get_rect(topleft = (randint(700, 950), 355)))
+                else: obstacle_rect_list.append(fly.get_rect(topleft = (randint(700, 950), 275)))
+        else:
+            if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                game_active = True
+                start_time = int(pygame.time.get_ticks() / 1000)
 
-    screen.blit(black_pearl, pearl_rect)
+    if game_active:
+        # Getting the images on the screen
+        screen.blit(sky, (0, 0))
+        screen.blit(black_pearl, pearl_rect)
+        screen.blit(sea, (0, 400))
 
-    screen.blit(sea, (0, 400))
+        # check for collision 
+        game_active = collisions(obstacle_rect_list)
+        '''if pearl_rect.colliderect(kraken_rect):
+            print("collision")
+            pygame.quit()
+            sys.exit()'''
 
-    kraken_rect.x -= 4
-    screen.blit(kraken, (kraken_rect.x, 355))
-    if kraken_rect.x < -60 : kraken_rect.x = 800
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        score = count_score()
 
-    # check for collision 
-    if pearl_rect.colliderect(kraken_rect):
-        print("collision")
-        pygame.quit()
-        sys.exit()
+    else:
+        screen.fill((94, 129, 162))
+        screen.blit(player, player_rect)
+        screen.blit(game_name, game_name_rect)
+        obstacle_rect_list.clear()
 
-    count_score()
+        score_message = font.render(f"Your score : {score}", False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 525))
+        
+        if score == 0: screen.blit(game_message, game_message_rect)
+        else: screen.blit(score_message, score_message_rect)
 
     pygame.display.update()
     clock.tick(60)
